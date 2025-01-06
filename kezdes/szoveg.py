31d0e28062189274d454d0f6a37a95a683b27dfc
Denes Szabo <donci@internode.hu>
2025-01-06 18:05:11 +0100

    Add text processor python

---
+++ kezdes/szoveg.py
@@ -0,0 +1,14 @@
+# Szövegkezelő Python program
+
+string1 = 'aaa'
+string2 = 'bbb'
+
+string = string1 + string2
+print(string)
+
+print('a' in string1)
+
+print(string1 * 3)
+
+print(string[-1])
+print(type(string[0]))
